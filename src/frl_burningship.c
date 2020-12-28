2e93c4f9cfa87e2cf8d3539efd0a87adda67b08d
kafkalainen <joonas.hj.nivala@gmail.com>
2020-12-28 13:03:15 +0200

    new -> z in burningship

--- src/frl_burningship.c
+++ src/frl_burningship.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 11:33:59 by jnivala           #+#    #+#             */
-/*   Updated: 2020/12/15 14:33:15 by jnivala          ###   ########.fr       */
+/*   Updated: 2020/12/28 12:58:55 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 void	frl_burningship(t_data *data, t_uv screen, t_cam *cam)
 {
 	t_complex		c;
-	t_complex		new;
+	t_complex		z;
 	t_complex		temp;
 	int				i;
 
 	i = 0;
 	c = frl_normalize_coordinates(screen.u, screen.v, cam);
-	new = c;
-	while (new.re * new.re + new.im * new.im < 4.0 && i < cam->max_iter)
+	z = c;
+	while (z.re * z.re + z.im * z.im < 4.0 && i < cam->max_iter)
 	{
-		temp.re = new.re * new.re - new.im * new.im + c.re;
-		new.im = ft_dabs(2.0 * new.re * new.im) + c.im;
-		new.re = temp.re;
+		temp.re = z.re * z.re - z.im * z.im + c.re;
+		z.im = ft_dabs(2.0 * z.re * z.im) + c.im;
+		z.re = temp.re;
 		i++;
 	}
 	g42_mlx_pixel_put(data, screen.u, screen.v, frl_colour_scheme(i, cam));
